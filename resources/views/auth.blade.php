5bfbc05655bac488d6caf76f65f9fe0ba4495a5f
Maysya Atshyfa Putri Widiyati <maysya2561@gmail.com>
2026-04-15 03:11:37 +0700

    tambah logo dan background

--- resources/views/auth.blade.php
+++ resources/views/auth.blade.php
@@ -1,186 +1,66 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login & Register</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <Style>
+        html,
+        body {
+            height: 100%;
+            margin: 0;
+        }
 
-<meta charset="UTF-8">
-<title>Hotel  MARstay </title>
-
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-
-<style>
-
-body{
-height:100vh;
-background: linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)),
-url("https://images.unsplash.com/photo-1566073771259-6a8506099945");
-background-size:cover;
-background-position:center;
-display:flex;
-align-items:center;
-justify-content:center;
-font-family:Arial;
-}
-
-.login-box{
-width:420px;
-background:white;
-border-radius:15px;
-padding:35px;
-box-shadow:0 10px 40px rgba(0,0,0,0.3);
-}
-
-.logo{
-font-size:28px;
-font-weight:bold;
-text-align:center;
-margin-bottom:10px;
-}
-
-.subtitle{
-text-align:center;
-color:gray;
-margin-bottom:25px;
-}
-
-.btn-login{
-background:#1a2b49;
-border:none;
-}
-
-.btn-login:hover{
-background:#668bca;
-border:none;
-}
-
-.footer{
-text-align:center;
-margin-top:20px;
-font-size:13px;
-color:gray;
-}
-
-.top-logo{
-position:absolute;
-top:20px;
-left:30px;
-width:230px;
-z-index:3;
-}
-
-.top-logo{
-position:absolute;
-top:20px;
-left:30px;
-width:230px;
-z-index:3;
-mix-blend-mode: multiply;
-}
-
-.top-logo{
-position:absolute;
-top:20px;
-left:30px;
-width:230px;
-z-index:3;
-filter: drop-shadow(0px 2px 6px rgba(0, 0, 0, 0));
-}
-
-#splash{
-position:fixed;
-top:0;
-left:0;
-width:100%;
-height:100%;
-background:#ffffff;
-display:flex;
-justify-content:center;
-align-items:center;
-z-index:9999;
-}
-
-.splash-logo{
-width:350px;
-animation: zoom 2s ease;
-}
-
-@keyframes zoom{
-0%{
-transform:scale(0.5);
-opacity:0;
-}
-
-100%{
-transform:scale(1);
-opacity:1;
-}
-}
-</style>
-
-
+        body {
+           //background: url('{{ asset('background login.png') }}') no-repeat center center fixed;//
+            background-size: cover;
+        }
+    </style>
 </head>
 
 <body>
-<div id="splash">
-    <img src="{{ asset('logo hotel.png') }}" class="splash-logo">
-</div>
+    <div class="container d-flex justify-content-end align-items-center vh-100 pe-5">
+        <div class="login-card p-$ shadow">
 
-<body>
+            <!---JUDUL--->
+            <div class="text-center mb-4">
+                <h5>Selamat datang di</h5>
+                <img src="{{ asset('MARSTAY LOGO.png') }}" alt="Logo MARStay" style="width:150px;" class="mb-2">
 
-<img src="{{ asset('logo hotel.png') }}" class="top-logo">
+                <small class="d-block text-center">
+                    Masuk untuk melanjutkan ke akun Anda
+                </small>
+            </div>
 
-<div class="overlay"></div>
+            <!---FORM--->
+            <form>
+                <div class="mb-3">
+                    <div class="input-group">
+                        <span class="input-group-text">
+                            <i class="bi bi-person"></i>
+                        </span>
+                        <input type="text" class="form-control" placeholder="Email atau Username">
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <div class="input-group">
+                        <span class="input-group-text">
+                            <i class="bi bi-lock"></i>
+                        </span>
+                        <input type="password" class="form-control" placeholder="Password">
+                        <span class="input-group-text">
+                            <i class="bi bi-eye"></i>
+                    </div>
+                </div>
+                <div class="d-flex justify-between mb-3">
+                    <div>
+                        <input type="checkbox"> Ingat saya </div>
 
-<div class="login-box">
-
-<div class="logo">
-Hotel  MARstay 
-</div>
 
 
-<form>
-
-<div class="mb-3">
-<label class="form-label">Email</label>
-<div class="input-group">
-<span class="input-group-text">
-<i class="bi bi-envelope"></i>
-</span>
-<input type="email" class="form-control" placeholder="Masukkan Email">
-</div>
-</div>
-
-<div class="mb-3">
-<label class="form-label">Password</label>
-<div class="input-group">
-<span class="input-group-text">
-<i class="bi bi-lock"></i>
-</span>
-<input type="password" class="form-control" placeholder="Masukkan Password">
-</div>
-</div>
-
-<div class="d-grid">
-<button class="btn btn-login text-white">
-Login
-</button>
-</div>
-
-</form>
-
-<div class="footer">
-Hotel MARstay 
-</div>
-
-</div>
-<script>
-
-setTimeout(function(){
-document.getElementById("splash").style.display="none";
-},3000);
-
-</script>
 </body>
+
 </html>
